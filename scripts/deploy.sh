1c33d73c569e83548c097fa32823fd1c9987d38a
David Vezzani <david_vezzani@yahoo.com>
2018-01-30 08:52:24 -0700

    manipulates unpackaged css to work with production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 path=/var/www/vhosts/welfie.co
+
 cp "$path/welfie-fe/dist/index.html" "$path/httpdocs"
 cp -r "$path/welfie-fe/dist/static/" "$path/httpdocs"
+
+mkdir -p "$path/httpdocs/static/assets/pure"
+cp "$path/welfie-fe/src/assets/pure/pure-min.css" "$path/httpdocs/static/assets/pure"
+cp "$path/welfie-fe/src/assets/pure/grids-responsive.css" "$path/httpdocs/static/assets/pure"
+cp "$path/welfie-fe/src/assets/pure/offsets.css" "$path/httpdocs/static/assets/pure"
+
+sed -i -e 's#src/assets#/static/assets#g' "$path/httpdocs/index.html"
